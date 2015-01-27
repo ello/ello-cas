4c0bcf588c4777ff1bd19f8c2492d6925fb36d84
Ryan Tyer <ryan.tyer@jivesoftware.com>
2015-01-27 12:13:49 -0700

    Updating some tests and queries to use the schema

--- lib/ello/cas.rb
+++ lib/ello/cas.rb
@@ -6,17 +6,37 @@ module Ello
 	module Cas
 		class Core
 			def initialize(keyspace: "ello_test")				
-				@cluster = Cassandra.cluster(				
+				@@cluster = Cassandra.cluster(				
 					logger: Logger.new($stderr), 
 					compression: :lz4,
 					hosts: ['127.0.0.1', '127.0.0.2', '127.0.0.3']
 				)
-				@cluster.connect(keyspace)
+				@session = @@cluster.connect(keyspace)
+
+				@friends_insert = @session.prepare(
+					"INSERT INTO ello_test.friends_activities(user_id, subject_id, subject_type, posted_at, updated_at, kind, originating_user_id) " \
+					"VALUES (?,?,?,now(),now(),?,?)"
+				)
+
+				@friends_select = @session.prepare(
+					"SELECT subject_id, subject_type, posted_at, kind, originating_user_id " \
+					"FROM ello_test.friends_activities " \
+					"WHERE user_id = ? LIMIT 10"
+				)
 			end
 
 			def hosts
-				@cluster.each_host.map { |host| host.ip }
+				@@cluster.each_host.map { |host| host.ip }
 			end
+
+			def friends_insert(user_id, subject_id, subject_type, kind, originating_user_id)
+				@session.execute(@friends_insert, user_id, subject_id, subject_type, kind, originating_user_id)
+			end
+
+			def friends_activities(user_id)
+				@session.execute(@friends_select, user_id)
+			end
+
 		end
 	end
 end
--- spec/cas_spec.rb
+++ spec/cas_spec.rb
@@ -10,6 +10,20 @@ module Ello
 				it 'should return the localhost host' do
 					expect(output.length).to be(3)
 					expect(output).to include('127.0.0.1')
+				end				
+			end
+
+			describe '#friends_activities' do
+				let(:user_id) { 15 }
+				let(:originating) { 25 }
+				before {
+					subject.friends_insert(user_id, 100, "Test Type", "Test Kind", originating)
+				}
+				let(:output) { subject.friends_activities(user_id).first }
+
+				it 'should return the record inserted' do 
+					expect(output['originating_user_id']).to eq(originating)
+					expect(output).to_not be_nil
 				end
 			end
 		end
