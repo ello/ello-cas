c730744e084cfbbb5241e3f23f4af5ff2f951b1f
Ryan Tyer <ryan.tyer@jivesoftware.com>
2015-02-21 18:18:15 -0700

    Added stream based schema and updated to cas/spec to support it.
    Used a compound partition key for stream.  Adjusted tests to generate uuid's for those streams at the start (new each time).  Running the rake task to rebuild the keyspace each run of rspec so we get a pristine environment.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,12 @@
 require 'ello/cas'
+require 'rake'
+
+# Probably overkill and should find a better way to accomplish this in the future. 
+
+load File.expand_path("../../tasks/cas.rake", __FILE__)
+
+RSpec.configure do |config|
+  config.before(:all) do
+  	Rake::Task['cas'].invoke('schema/schema_streams.cql')
+  end
+end
